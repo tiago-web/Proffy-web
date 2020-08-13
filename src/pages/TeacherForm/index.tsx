ba8c76b5b3da6c339ed3e58fa7fec74abd5fcce5
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-08-12 20:23:23 -0400

    Add routes and finish PageHeader component

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
 
-const TeacherForm: React.FC = () => <h1>Teacher Form</h1>;
+const TeacherForm: React.FC = () => (
+	<div id="page-teacher-form" className="container">
+		<PageHeader title="Que incrível que você quer dar aulas." />
+	</div>
+);
 
 export default TeacherForm;
